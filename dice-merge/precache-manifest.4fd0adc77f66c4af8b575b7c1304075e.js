58269c1e8a61b42443c194b0a2ef0dea0a1e0869
Bastien WEBER <bweber@wynd.eu>
2019-10-25 15:15:25 +0200

    🎲 Add dice-merge

---
+++ dice-merge/precache-manifest.4fd0adc77f66c4af8b575b7c1304075e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93340b8ec112038f89d1deea2f0f3cc3",
+    "url": "/dice-merge/index.html"
+  },
+  {
+    "revision": "f640e69e76a1e69463ab",
+    "url": "/dice-merge/static/css/main.ee80a495.chunk.css"
+  },
+  {
+    "revision": "3f3c8d9931b0dfb10f28",
+    "url": "/dice-merge/static/js/2.948ed607.chunk.js"
+  },
+  {
+    "revision": "f640e69e76a1e69463ab",
+    "url": "/dice-merge/static/js/main.e757c778.chunk.js"
+  },
+  {
+    "revision": "290c325142bec6464bca",
+    "url": "/dice-merge/static/js/runtime-main.84d8ec69.js"
+  }
+]);
